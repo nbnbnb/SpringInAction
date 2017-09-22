61bcb916ff4ff06a6ae41c1d323c23d922d9479c
ZhangJin <nbnbnb.zhang@gmail.com>
2017-09-22 23:04:12 +0800

    Remark

--- common/src/main/java/filter/SetCharacterEncodingFilter.java
+++ common/src/main/java/filter/SetCharacterEncodingFilter.java
@@ -7,7 +7,13 @@ import java.io.IOException;
 //        filterName = "SetCharacterEncodingFilter",
 //        urlPatterns = {"/*"},
 //        initParams = {@WebInitParam(name = "encoding", value = "utf-8")})
-// 显式在 Web.xml 中配置
+
+// 创建自定义的编码过滤器
+// 可以使用 @WebFilter 标记，这样引用了包的项目将会自动配置
+// 也可以不指定此标记，所以需要显式在 Web.xml 中配置
+
+// Spring 框架自动配置了编码，不需要此处进行显式编码指定
+// 此处仅仅是 Demo
 public class SetCharacterEncodingFilter implements Filter {
 
     private String encoding = null;
